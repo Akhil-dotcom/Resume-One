dbeea855da418ff4acd7e5b812ac986cf2fc114b
unknown <akhilrajktt@gmail.com>
2022-01-31 14:38:59 +0530

    skills commit

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,17 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+type Google = {
+  clientId: string | undefined,
+  clientSecret: string | undefined
+}
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
+    // GoogleProvider<Google>({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET,
+    // }),
     // ...add more providers here
   ],
 })
